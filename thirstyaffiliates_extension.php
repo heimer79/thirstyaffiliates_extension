02acfe251ceca2a38e6835b8bc2904943082c68b
Heimer Martinez <hmartinez@cafetosoftware.com>
2023-07-14 12:04:16 -0500

    changed validation to pro plugin is installed

--- thirstyaffiliates_extension.php
+++ thirstyaffiliates_extension.php
@@ -3,7 +3,9 @@
 Plugin Name: ThirstyAffiliates Extended
 Description: Extiende la funcionalidad del plugin ThirstyAffiliates para manejar cadenas de consulta especiales en las URL de redirección.
 Author: Heimer Martinez
-Version: 1.0
+Author URI: https://github.com/heimer79/thirstyaffiliates_extension
+Version: 1.0.1
+Text Domain: thirstyaffiliates-pro
 */
 
 // Asegurémonos de que no estamos exponiendo ninguna información si se llama directamente
@@ -11,10 +13,12 @@ if (!defined('ABSPATH')) {
     exit;
 }
 
-// Verificamos si ThirstyAffiliates está activo
+// Verificamos si ThirstyAffiliates pro está activo
 include_once(ABSPATH . 'wp-admin/includes/plugin.php');
 
-if (is_plugin_active('thirstyaffiliates/thirstyaffiliates.php')) {
+$plugin_slug = 'thirstyaffiliates-pro/thirstyaffiliates-pro.php';
+
+if (is_plugin_active($plugin_slug)) {
 
     function ta_extended_filter_redirect_url($redirect_url, $thirstylink, $query_string = '') {
 
@@ -23,8 +27,6 @@ if (is_plugin_active('thirstyaffiliates/thirstyaffiliates.php')) {
 
         if (!$query_string || !$thirstylink->is('pass_query_str'))
             return $redirect_url;
-
-
 
         // Parsear la URL
         $parsed_url = parse_url($redirect_url);
@@ -35,31 +37,33 @@ if (is_plugin_active('thirstyaffiliates/thirstyaffiliates.php')) {
         $path = $parsed_url['path'];
         $query = $parsed_url['query'];
 
-
         // Descomponemos la cadena de consulta en sus componentes
         $query_components = explode('=', $query);
-
-
-
 
         // Agregamos el nombre del parámetro y su valor al array $query_params
         $param_name = $query_components[0]; //nombre de la variable en la url ejemplo: lang
         $param_value = $query_components[1]; //Valor de la varieble ejemplo: es
 
-
         $redirect_url = $scheme . '://' . $host . $path;
-
 
         $connector  = (strpos($redirect_url, '?') === false) ? '?' : '';
 
-
         if (strpos($query, '?') !== false) {
             $query_string = ltrim($query_string, '?');
-
             return  $redirect_url . $connector . $param_name . '=' . $query_string;
         } else {
             return  $redirect_url . $connector . $param_name . '=' . '&' . $query_string;
         }
     }
     add_filter('ta_filter_redirect_url', 'ta_extended_filter_redirect_url', 20, 3);
+} else {
+    // El plugin no está activo, mostramos un mensaje en el panel de administración.
+    add_action('admin_notices', 'my_admin_notice');
+    function my_admin_notice() {
+?>
+        <div class="notice notice-warning is-dismissible">
+            <p><?php _e('ThirstyAffiliates Pro plugin is required!', 'thirstyaffiliates-pro'); ?></p>
+        </div>
+<?php
+    }
 }
